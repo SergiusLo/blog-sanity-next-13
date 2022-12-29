fd534a23326499db5bca276c44475f3c4f084a8d
SergiusLo <loboyko99911@gmail.com>
2022-12-30 01:48:02 +0200

    studio
    studio sanity.config.ts rename

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,16 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 
-export default defineConfig({
-  name: "default",
-  title: "nextjs13 sanity blog",
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
-  projectId: "752ymkfa",
-  dataset: "production",
+export default defineConfig({
+  basePath: "/studio",
+  name: "Sergius_Content_Studio",
+  title: "Sergius Content Studio",
+
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
