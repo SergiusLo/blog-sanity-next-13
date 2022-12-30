e44fd8239de5c570114862f86cb04b533009b378
SergiusLo <loboyko99911@gmail.com>
2022-12-30 19:42:31 +0200

    admin panel customs

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
+import StudioNavbar from "./components/StudioNavbar";
+import Logo from "./components/Logo";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -18,5 +20,11 @@ export default defineConfig({
 
   schema: {
     types: schemaTypes,
+  },
+  studio: {
+    components: {
+      logo: Logo,
+      navbar: StudioNavbar,
+    },
   },
 });
---
+++ theme.ts
@@ -0,0 +1,38 @@
+import { buildLegacyTheme } from "sanity";
+
+const props = {
+  "--my-white": "#fff",
+  "--my-black": "#1a1a1a",
+  "--brand": "#F7AB0A",
+  "--my-red": "#db4437",
+  "--my-yellow": "#f4b400",
+  "--my-green": "#0f9d58",
+};
+
+// export const myTheme = buildLegacyTheme
+//                                               ({
+
+//   "--black": props("--sy-black"),
+//   "--white": props("--sy-white"),
+//    "--gray": "#666",
+//   "--gray-base": "#666",
+//   "--component-bg":
+//   "-component-text-color": props["--ny-shite"),
+//   / Brand w/
+//   "--brand-primary": props ("-papafan-brand"),
+
+//   "--default-button-color": "#666",
+//   "--default-button-primary-color": props("-papafan-brand"),
+//   "--default-button-success-color": props ("--my-green"),
+//   "--default-button-warning-color": props ("-my-yellow"),
+//   "--default-button-danger-color": props["-my-red"],
+
+//   "--state-info-color": props ("-papafae-brand"),
+//   "--state-success-color": props["--ay-green"),
+//   "--state-varning-color": props("--ay-yellow"),
+//   "--state-danger-color": props["-my-red"),
+
+//                             props["--ay-black"],
+//   "--main-navigation-color-inverted": props["-sy-white"],
+//   "--focus-color": props["--papafam-brand"],
+//                                               });
